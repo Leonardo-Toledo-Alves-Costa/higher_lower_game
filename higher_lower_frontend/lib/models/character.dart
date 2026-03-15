f7351f5f8a62882612eb36641739119e1765d74d
Leonardo <d2025010842@unifei.edu.br>
2026-03-15 00:38:17 -0300

    Add Character model with properties and constructor

---
+++ higher_lower_frontend/lib/models/character.dart
@@ -0,0 +1,13 @@
+class Character {
+  final String name;
+  final int followerCount;
+  final String description;
+  final String country;
+
+  Character({
+    required this.name,
+    required this.followerCount,
+    required this.description,
+    required this.country,
+  });
+}
