686e2e7da2109222ef7bd53133dd530f7737ae80
Leonardo <d2025010842@unifei.edu.br>
2026-03-15 00:57:39 -0300

    Add ApiService for fetching character data and update dependencies

--- higher_lower_frontend/lib/models/character.dart
+++ higher_lower_frontend/lib/models/character.dart
@@ -10,4 +10,13 @@ class Character {
     required this.description,
     required this.country,
   });
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return Character(
+      name: json['name'],
+      followerCount: json['follower_count'],
+      description: json['description'],
+      country: json['country'],
+    );
+  }
 }
